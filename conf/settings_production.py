cd6d7ad3d9e17af47e1ce8d32ea4a3f9e6e90bca
huziyue <huziyue@zork.com.cn>
2018-11-23 10:59:44 +0800

    修改数据库配置

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -19,10 +19,10 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': '',                            # 你的数据库user
-        'PASSWORD': '',                        # 你的数据库password
-        'HOST': '',                   		   # 数据库HOST
+        'NAME': 'huziyue_0',                        # 数据库名 (默认与APP_ID相同)
+        'USER': 'root',                        # 你的数据库user
+        'PASSWORD': '@86Dj3Y(it ',                        # 你的数据库password
+        'HOST': '10.0.1.29 ',                   # 开发的时候，使用localhost
         'PORT': '3306',                        # 默认3306
     },
 }
